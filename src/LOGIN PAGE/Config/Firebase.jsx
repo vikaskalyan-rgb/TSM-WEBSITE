b1af5a736a7da90b6dc25645b15b121dc58bc014
Kevin-Richard-A <75072776+Kevin-Richard-A@users.noreply.github.com>
2022-06-18 17:04:57 +0530

    Delete src directory

--- src/LOGIN PAGE/Config/Firebase.jsx
+++
@@ -1,8 +0,0 @@
-import {initializeApp} from 'firebase/app';
-import { getAuth} from 'firebase/auth';
-import firebaseConfig from "./firebaseConfig";
-
-const firebaseApp =initializeApp(firebaseConfig) 
-const auth =getAuth(firebaseApp)
-
-export {auth};
